aa4921ce1571261da4c1c1a6157b5cd561fd8cec
hooghub <goookuooog@gmail.com>
2025-10-31 08:01:14 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# 生成 UUID
 if [ -z "$UUID" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
   echo "Generated UUID: $UUID"
 fi
 
+# 替换 config.json 中的 {{UUID}}
 sed -i "s/{{UUID}}/$UUID/g" /etc/v2ray/config.json
 
-exec v2ray -config /etc/v2ray/config.json
+# 启动 V2Ray v5
+exec v2ray run -c /etc/v2ray/config.json
