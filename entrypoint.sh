6a3d1835bc71507902c133ce3dd535e25e07ccb7
hooghub <goookuooog@gmail.com>
2025-10-31 07:50:41 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-# 自动生成随机 UUID，如果环境变量没有指定
-UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
+# 如果没有 UUID，生成一个
+if [ -z "$UUID" ]; then
+  UUID=$(cat /proc/sys/kernel/random/uuid)
+  echo "Generated UUID: $UUID"
+fi
 
-# 替换 config.json 中的 UUID
-sed -i "s|{{UUID}}|$UUID|g" /etc/v2ray/config.json
+# 替换 config.json 中的占位符
+sed -i "s/{{UUID}}/$UUID/g" /etc/v2ray/config.json
 
-# 启动 V2ray
-v2ray -config /etc/v2ray/config.json
+# 启动 V2Ray
+exec v2ray -config /etc/v2ray/config.json
